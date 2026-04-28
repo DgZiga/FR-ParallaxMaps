e937659cd42731c8fc0470ee1ec578a5ca1562ef
avon <dgziga@gmail.com>
2026-04-28 18:30:44 +0200

    Fixed bg suddenly sliding

--- src/main.c
+++ src/main.c
@@ -1,21 +1,39 @@
 
-typedef unsigned char u8;
-typedef unsigned short int u16;
-
 #define REG_ID_BG3HOFS 0x1C
 #define REG_ID_BG3VOFS 0x1E
-extern void lcd_io_set(u8 reg_id, u16 value);
+struct fieldCameraStruct{
+    s16 xPixelOffset;
+    s16 yPixelOffset;
+};
+
+extern void lcd_io_set(u8 reg_id, s16 value);
+extern u16 lcd_io_get(u8 reg_id);
 u16 var_load(u16 variable);
 
-void main(u16 r4, u16 r5){
+extern struct fieldCameraStruct party_opponent;
+extern s16 sHorizontalCameraPan;
+extern s16 sVerticalCameraPan;
+
+void doPrint(s32 a, s32 b, s32 c){
+    dprintf("%x, %x, %x\n", a, b, c);
+}
+
+void main(u8 r4, u8 r5){
     u16 bg3_scale = var_load(0x4000);
-    
-    u16 bg3_hofs = r5;
-    u16 bg3_vofs = r4;
+
+    u8 bg3_hofs = r5;
+    u8 bg3_vofs = r4;
+
+    s32 expandedH = (s32)bg3_hofs;
+    s32 expandedV = (s32)bg3_vofs;
 
     if (bg3_scale == 1 || bg3_scale == 2) {
-        bg3_hofs = bg3_hofs >> bg3_scale;
-        bg3_vofs = bg3_vofs >> bg3_scale;
+        s16 r5New = party_opponent.xPixelOffset + sHorizontalCameraPan;
+        s16 r4New = sVerticalCameraPan + party_opponent.yPixelOffset + 8;
+        s8 halvedH = r5New / 2;
+        s8 halvedV = r4New / 2;
+        bg3_hofs = halvedH;
+        bg3_vofs = halvedV;
     }
     else if (bg3_scale == 3) {
         bg3_hofs = 0;
--- src/main.s
+++ src/main.s
@@ -2,27 +2,95 @@
 .thumb
 .open "./BPRE0.gba","./out.gba", 0x08000000
 
-
+//change scrolling amount
 .org 0x0805A636
 .thumb
 .align 2
-LDR r0, =test|1
+LDR r0, =changeScrollAmt|1
 BX r0
 .pool
 
+//make it so coords8_add is actually coords16_add so that we can properly divide by 2
+.org 0x0805AC7E
+.thumb
+.align 2
+LDR r0, =addCoords16|1
+BX r0
+.pool
+//reset coords16 on map update
+.org 0x0805A5A8
+.thumb
+.align 2
+LDR r0, =resetCoords16|1
+BX r0
+.pool
 
 .org freespace
 .importobj "./build/linked.o"
 
-test:
+changeScrollAmt:
 MOV r0, R4
 MOV r1, R5
 BL main
 
-back_to_normal:
+changeScrollAmt_back_to_normal:
 POP {R4,R5}
 POP {R0}
 BX r0
+
+tilemap_camera_move_something equ 0x03000E90
+nu_x equ 0x0300506C
+free_ram equ 0x0202402C //using opponents pkmn as free ram
+
+addCoords16:
+LDR     R0, =tilemap_camera_move_something
+MOV     R1, R7
+LDR     R2, =free_ram
+
+//taken straight from 0805A5D4 coords8_add
+LDRH    R3, [R2]
+ADD     R3, R3, R1
+
+
+STRH    R3, [R2]
+
+STRB    R3, [R0]
+LDRH    R1, [R2,#2]
+MOV     R2, R8
+ADD     R1, R1, R2
+STRB    R1, [R0,#1]
+
+LDR     R2, =free_ram
+STRH    R1, [R2,#2]
+MOV     R2, R8
+
+LDR     R1, =nu_x
+LDRH    R0, [R1]
+SUB     R0, R0, R7
+STRH    R0, [R1]
+LDR     R1, =0x0805AC90|1
+BX      R1
+
+resetCoords16:
+LDR     R0, =tilemap_camera_move_something
+MOV     R1, #0
+STRB    R1, [R0,#2]
+STRB    R1, [R0,#3]
+STRB    R1, [R0]
+STRB    R1, [R0,#1]
+MOV     R1, #1
+STRB    R1, [R0,#4]
+
+LDR     R0, =free_ram
+MOV     R1, #0
+STR     R1, [R0]
+
+BX LR
+/*
+POP     {R0}
+BX      R0
+*/
+
 
 .pool
 
